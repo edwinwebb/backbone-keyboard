96c50cbe1c74c49f9670a341dfc79b968cba1f75
Edwin Webb <edwin.webb@gmail.com>
2013-03-21 12:02:52 +0000

    Added multi-touch support

--- src/backbone-keyboard.js
+++ src/backbone-keyboard.js
@@ -40,12 +40,31 @@ Backbone.View.Keyboard = Backbone.View.extend({
 
   //will be populated with options.targetSelector
   targ : undefined,
+  
   //will be poulated with the actions from special keys
   actions : {},
 
+  //enable touch events
+  isTouch : (function() {
+    try {
+     document.createEvent("TouchEvent");
+     return true;
+    } catch (e) {
+     return false;
+    }
+  }()),
+
   //click a key
-  events : {
-    'click .key' : 'clickHandler'
+  events : function() {
+    if(this.isTouch) {
+      return {
+        'touchstart .key' : 'touchStartHandler',
+      }
+    } else {
+      return {
+        'click .key' : 'clickHandler',
+      }
+    }
   },
 
   //merge options and auto-render
@@ -86,18 +105,33 @@ Backbone.View.Keyboard = Backbone.View.extend({
   //click on a .key
   clickHandler : function(e) {
 
-    var targ = e.currentTarget;
-    var val = this.targ.val() || "";
-
-    //if special, look up the action and apply it to val
-    if(targ.hasAttribute('data-action')) {
-      this.targ.val(this.actions[targ.getAttribute('data-action')](val));
-    } else {
-      this.addChar(e.currentTarget.innerText);  
-    }
+    var targ = e.currentTarget;  
 
     e.preventDefault();
 
+    this.keyHandler(targ);
+
+  },
+
+  touchStartHandler : function(e) {
+    var touches = e.originalEvent.targetTouches;
+
+    var val = this.targ.val() || "";
+
+    _.each(e.originalEvent.targetTouches, function(touch) {
+      this.keyHandler(touch.target);
+    }, this);
+  },
+
+  keyHandler : function(keyEl) {
+
+    var val = this.targ.val() || "";
+
+    if(keyEl.hasAttribute('data-action')) {
+      this.targ.val(this.actions[keyEl.getAttribute('data-action')](val));
+    } else {
+      this.addChar(keyEl.innerText);  
+    }
   },
 
   //sets the keyboards target element
